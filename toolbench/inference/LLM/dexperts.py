2b6f0d585ba5d530286719bfe5920cf4f219ed41
kolber <kolber@cl.uni-heidelberg.de>
2024-11-23 13:30:00 +0100

    Fix resize_token_embeddings

--- toolbench/inference/LLM/dexperts.py
+++ toolbench/inference/LLM/dexperts.py
@@ -91,9 +91,9 @@ class DExpertsLlama:
         new_num_tokens: Optional[int] = None,
         pad_to_multiple_of: Optional[int] = None,
     ) -> torch.nn.Embedding:
-        self.base.resize_token_embeddings(new_num_tokens, pad_to_multiple_of)
-        self.expert.resize_token_embeddings(new_num_tokens, pad_to_multiple_of)
-        self.antiexpert.resize_token_embeddings(new_num_tokens, pad_to_multiple_of)
+        self.base.resize_token_embeddings(new_num_tokens)
+        self.expert.resize_token_embeddings(new_num_tokens)
+        self.antiexpert.resize_token_embeddings(new_num_tokens)
 
     def _get_tokenized_chat_inputs(self, input_ids):
         """Decode input_ids and encode again to insert chat formatting"""
